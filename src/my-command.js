661c4834068fa31976fe177f5a39de2658f85276
Kevin Gutowski <kgutowski@berkeley.edu>
2020-04-09 20:13:30 -0700

    Trying to figure out how to update state on startup properly

--- src/my-command.js
+++ src/my-command.js
@@ -5,13 +5,61 @@ let Settings = sketch.Settings
 const prefString = "tryToFitToPixelBounds"
 
 export function startup() {
+    console.log('PixelFitted | SKETCH STARTING UP')
     let isPixelFitted = Settings.globalSettingForKey(prefString)
     let pixelFittedMenuItem = getMenuItem()
 
-    if (isPixelFitted) {
-        pixelFittedMenuItem.state = NSControlStateValueOn
+    console.log('PixelFitted | 1 isPixelFitted: ' + isPixelFitted)
+    try {
+        console.log('PixelFitted | 1' + pixelFittedMenuItem.description())
+    } catch(e) {
+        console.log('PixelFitted | 1 ERROR')
+        console.log(e)
+    }
+
+    if (!pixelFittedMenuItem) {
+        console.log("PixelFitted | No menu item")
+        setTimeout(
+            function(){
+                console.log('PixelFitted | How about now?')
+                console.log('PixelFitted | 2 isPixelFitted: ' + isPixelFitted)
+                isPixelFitted = Settings.globalSettingForKey(prefString)
+                pixelFittedMenuItem = getMenuItem()
+                try {
+                    console.log('PixelFitted | 3' + pixelFittedMenuItem.description())
+                } catch (e) {
+                    console.log('PixelFitted | 3 ERROR')
+                    console.log(e)
+                }
+                console.log('PixelFitted | 3 isPixelFitted: ' + isPixelFitted)
+                setMenuItemState(pixelFittedMenuItem, isPixelFitted)
+            }, 1000);
     } else {
-        pixelFittedMenuItem.state = NSControlStateValueOff
+        setMenuItemState(pixelFittedMenuItem, isPixelFitted)
+    }
+}
+
+function setMenuItemState(menuItem, settingState) {
+    console.log('PixelFitted | Setting state: ' + settingState)
+    console.log('PixelFitted | menuItem: ' + menuItem.description())
+    console.log('PixelFitted | menuItemState: ' + menuItem.state())
+    if (settingState) {
+        console.log("PixelFitted | Setting On state")
+        try {
+            menuItem.state = NSControlStateValueOn
+            let menu = NSApplication.sharedApplication().mainMenu()
+            let pluginsMenu = menu.itemWithTitle('Plugins').submenu()
+            menu.update()
+            pluginsMenu.update()
+            NSApplication.sharedApplication().mainMenu().update()
+        } catch (e) {
+             console.log("PixelFitted | couldn't set state")
+            console.log(e)
+        }
+        console.log("PixelFitted | MenuItem State After: " + menuItem.state())
+    } else {
+        console.log("PixelFitted | Setting Off state")
+        menuItem.state = NSControlStateValueOff
     }
 }
 
@@ -19,6 +67,7 @@ function getMenuItem() {
     let menu = NSApplication.sharedApplication().mainMenu()
     let pluginsMenu = menu.itemWithTitle('Plugins').submenu()
     let menuItem = pluginsMenu.itemWithTitle('Pixel fitting')
+    console.log('PixelFitted | getMenuItem: ' + menuItem)
     return menuItem
 }
 
@@ -28,10 +77,18 @@ export function toggleSetting() {
     if (pixelFittedMenuItem.state() == NSControlStateValueOff) {
         Settings.setGlobalSettingForKey(prefString, 1)
         pixelFittedMenuItem.state = NSControlStateValueOn
-        sketch.UI.message("✅ Pixel fitting on")
+        try {
+            sketch.UI.message("✅ Pixel fitting on")
+        } catch {
+            // likely no document open
+        }
     } else {
         Settings.setGlobalSettingForKey(prefString,0)
         pixelFittedMenuItem.state = NSControlStateValueOff
-        sketch.UI.message("🔴 Pixel fitting off")
+        try {
+            sketch.UI.message("🔴 Pixel fitting off")
+        } catch {
+            // likely no document open
+        }
     }
 }
